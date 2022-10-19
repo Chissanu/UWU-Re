0cdfbaa003633116efd6236699a8cc5d82364bd9
unknown <chissanuoak2@gmail.com>
2022-10-19 19:32:00 +0700

    Add function to look up from database

--- src/python/Database/dbInsert.py
+++ src/python/Database/dbInsert.py
@@ -1,16 +1,16 @@
 import psycopg2
 
 command = """ INSERT INTO drink_tables(drinkName,drinkList) VALUES(%s,%s) """
+#establishing the connection
+con = psycopg2.connect(
+   database="uwure",
+   user='admin',
+   password='uwure',
+   host='127.0.0.1',   
+   port= '5432'
+)
 
 def insertDrink(val):
-   #establishing the connection
-   con = psycopg2.connect(
-      database="uwure",
-      user='admin',
-      password='uwure',
-      host='127.0.0.1',   
-      port= '5432'
-   )
    #Creating a cursor object using the cursor() method
    cur = con.cursor()
    
@@ -19,6 +19,18 @@ def insertDrink(val):
    con.commit()
    #Close connection
    con.close()
+   
+def getDrink():
+   cur = con.cursor()
+   name = input("What drink are you looking for? ")
+   sql = "SELECT * FROM drink_tables where drinkName like '{drinkName}'".format(drinkName = name)
+   cur.execute(sql)
+   data = cur.fetchall()
+   if not data:
+      print("Could not find drink with that name")
+   else:
+      print(data)
+   
    
 
 def main():
@@ -33,4 +45,5 @@ def main():
    insertDrink(val)
    print(drinkList)
    
-main()
+#main()
+getDrink()
