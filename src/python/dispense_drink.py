5ece1f9ead20dfadb9a04a1c83f889f473a28100
Chissanu <chissanuoak2@gmail.com>
2022-10-24 18:22:59 +0700

    Add dummy data and switch to customTkinter

--- src/python/dispense_drink.py
+++ src/python/dispense_drink.py
@@ -1,6 +1,40 @@
 import myQueue
 
-requestInput = int(input("enter mode: "))
-if requestInput == 1:
-    drinkInput = input("enter drink: ")
+dummyData = [{"drinkName": "CustomDrink1",
+              "drinkID": 1,
+              "drinkList": ["Juice","Tea","Coffee","Cider","Sodar","Water"],
+              "drinkOrder": [20,123,12,45,23,44]
+              },
+             {"drinkName": "CustomDrink2",
+              "drinkID": 2,
+              "drinkList": ["Juice","Tea","Coffee","Cider","Sodar","Water"],
+              "drinkOrder": [20,123,12,45,23,44]
+              },
+             {"drinkName": "CustomDrink3",
+              "drinkID": 3,
+              "drinkList": ["Juice","Tea","Coffee","Cider","Sodar","Water"],
+              "drinkOrder": [20,123,12,45,23,44]
+              },
+             {"drinkName": "CustomDrink4",
+              "drinkID": 4,
+              "drinkList": ["Juice","Tea","Coffee","Cider","Sodar","Water"],
+              "drinkOrder": [20,123,12,45,23,44]
+              },
+             {"drinkName": "CustomDrink5",
+              "drinkID": 5,
+              "drinkList": ["Juice","Tea","Coffee","Cider","Sodar","Water"],
+              "drinkOrder": [20,123,12,45,23,44]
+              },
+             {"drinkName": "CustomDrink6",
+              "drinkID": 6,
+              "drinkList": ["Juice","Tea","Coffee","Cider","Sodar","Water"],
+              "drinkOrder": [20,123,12,45,23,44]
+              },]
+              
+
+
+print(dummyData)
+# requestInput = int(input("enter mode: "))
+# if requestInput == 1:
+#     drinkInput = input("enter drink: ")
     
--- src/python/main.py
+++ src/python/main.py
@@ -1,42 +1,80 @@
+from cgitb import text
 import tkinter as tk
-import requests
+from turtle import width
+import customtkinter as ctk
 
-root = tk.Tk()
+ctk.set_appearance_mode("System")  # Modes: "System" (standard), "Dark", "Light"
+ctk.set_default_color_theme("dark-blue")  # Themes: "blue" (standard), "green", "dark-blue"
 
-#Screen Config
-SCREEN_WIDTH = "1024"
-SCREEN_HEIGHT = "600"
-
-root.geometry(f"{SCREEN_WIDTH}x{SCREEN_HEIGHT}")
-root.title("UWU:RE")
-root.config(bg="light blue")
-root.grid_columnconfigure(0,weight=1)
-#root.attributes('-fullscreen', True)
-root.bind('<Escape>',lambda e: quit(e))
-
-def quit(e):
-    global root
-    root.destroy()
+class App(ctk.CTk):
+    def __init__(self):
+        super().__init__()
+        WIDTH = 1024
+        HEIGHT = 600
+        self.title("UWU:Reborn from Ashes")
+        self.geometry(f"{WIDTH}x{HEIGHT}")
+        self.bind('<Escape>',lambda e: quit(e))
+        self.config(bg="#6482EB")
+        self.resizable(False, False) 
+        
+        """
+        ======================================
+                    SELECT FRAME
+        ======================================
+        """
+        #Frame & Canvas creation
+        self.mainFrame = ctk.CTkFrame(self,width=WIDTH,height=HEIGHT,fg_color="#6482EB")
+        self.mainCanvas = ctk.CTkCanvas(self.mainFrame,width=WIDTH,height=HEIGHT,bg="#6482EB",highlightthickness=0)
+        
+        #Logo Label
+        self.nameLabel1 = ctk.CTkLabel(self.mainCanvas,text="UwU:",text_font=("Inter",130))
+        self.nameLabel1.place(x=200,y=123)
+        self.nameLabel2 = ctk.CTkLabel(self.mainCanvas,text="Re",text_font=("Inter",130),text_color="black")
+        self.nameLabel2.place(x=620,y=123)
+        self.nameLine = self.mainCanvas.create_line(200,315,840,315, fill="white", width=10)
+        
+        #Buttons
+        self.browseBtn = ctk.CTkButton(self.mainCanvas,width=248,
+                                       height=70,
+                                       text="Browse",
+                                       text_font=("Inter",30, 'bold'),
+                                       text_color="black",
+                                       corner_radius=30,
+                                       hover_color=("#2F8C3D"),
+                                       fg_color="#4BD960",
+                                       command=lambda :self.change_frame(self.mainFrame,"browse"))
+        self.browseBtn.place(x=98,y=380)
+        
+        """
+        ======================================
+                    Browse FRAME
+        ======================================
+        """
+        #Frame Creation
+        self.test = ctk.CTkFrame(self,width=WIDTH,height=HEIGHT,fg_color="red")
+        
+        """
+        ======================================
+                    Packing Main Frames
+        ======================================
+        """
+        #Pack Frame & Canvas
+        self.mainCanvas.pack(fill="both", expand=1)
+        self.mainFrame.pack(fill="both", expand=1)
     
-def changeScreen(oldFrame,screen):
-    oldFrame.pack_forget()
-    if screen == "select":
-        selectScreen()
+    def change_frame(self,oldFrame,newFrame):
+        oldFrame.pack_forget()
+        if newFrame == "browse":
+            self.test.pack()
+        print(newFrame)
+
+    def button_callback(self):
+        print("button pressed")
     
-def selectScreen():
-    newFrame = tk.Frame(root, width=SCREEN_WIDTH, height=SCREEN_HEIGHT,bg='red')
-    newFrame.pack(fill='both',expand=True)
+    def quit(self,e):
+        self.destroy()
 
-def mainScreen():
-    global root
-    mainFrame = tk.Frame(root, width=SCREEN_WIDTH, height=SCREEN_HEIGHT,bg='light blue')
-    mainFrame.pack(fill='both',expand=True)
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
 
-    #Project Name
-    name = tk.Label(mainFrame,text="UWU:RE",font=("Ariel",70),bg='light blue').pack(pady=50)
-
-    #Make Drink Button
-    makeDrinkBtn = tk.Button(mainFrame,font=("Ariel",20),text="Make Drink",command=  lambda : changeScreen(mainFrame,"select")).pack(padx=150,pady=150,anchor='w')
-
-mainScreen()
-root.mainloop()
