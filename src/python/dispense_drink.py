dffbbf9953640be2bf13f0b003b89967a6e4bd7e
Chissanu <chissanuoak2@gmail.com>
2022-10-29 17:10:30 +0700

    Switch from mL based to pump times based

--- src/python/dispense_drink.py
+++ src/python/dispense_drink.py
@@ -6,37 +6,31 @@ import time
 dummyData = [{"drinkName": "CustomDrink1",
               "drinkID": 1,
               "drinkList": ["Juice","Tea","Coffee","Cider","Sodar","Water"],
-              "drinkOrder": [20,123,12,45,23,44],
               "timesPressed": [1,2,1,3,4,3]
               },
              {"drinkName": "CustomDrink2",
               "drinkID": 2,
               "drinkList": ["Juice","Tea","Coffee","Cider","Sodar","Water"],
-              "drinkOrder": [20,123,12,45,23,44],
               "timesPressed": [1,2,1,3,4,3]
               },
              {"drinkName": "CustomDrink3",
               "drinkID": 3,
               "drinkList": ["Juice","Tea","Coffee","Cider","Sodar","Water"],
-              "drinkOrder": [20,123,12,45,23,44],
               "timesPressed": [1,2,1,3,4,3]
               },
              {"drinkName": "CustomDrink4",
               "drinkID": 4,
               "drinkList": ["Juice","Tea","Coffee","Cider","Sodar","Water"],
-              "drinkOrder": [20,123,12,45,23,44],
               "timesPressed": [1,2,1,3,4,3]
               },
              {"drinkName": "CustomDrink5",
               "drinkID": 5,
               "drinkList": ["Juice","Tea","Coffee","Cider","Sodar","Water"],
-              "drinkOrder": [20,123,12,45,23,44],
               "timesPressed": [1,2,1,3,4,3]
               },
              {"drinkName": "CustomDrink6",
               "drinkID": 6,
               "drinkList": ["Juice","Tea","Coffee","Cider","Sodar","Water"],
-              "drinkOrder": [20,123,12,45,23,44],
               "timesPressed": [1,2,1,3,4,3]
               },]
 
