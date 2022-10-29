74749d51adf3c5633814b6bb3ba8d0384cd23639
Tonkhaowm8 <Tonkhaowm8@gmail.com>
2022-10-29 14:51:43 +0700

    add enqueue code

--- src/python/dispense_drink.py
+++ src/python/dispense_drink.py
@@ -4,38 +4,44 @@ import myQueue
 dummyData = [{"drinkName": "CustomDrink1",
               "drinkID": 1,
               "drinkList": ["Juice","Tea","Coffee","Cider","Sodar","Water"],
-              "drinkOrder": [20,123,12,45,23,44]
+              "drinkOrder": [20,123,12,45,23,44],
+              "timesPressed": [1,2,1,3,4,3]
               },
              {"drinkName": "CustomDrink2",
               "drinkID": 2,
               "drinkList": ["Juice","Tea","Coffee","Cider","Sodar","Water"],
-              "drinkOrder": [20,123,12,45,23,44]
+              "drinkOrder": [20,123,12,45,23,44],
+              "timesPressed": [1,2,1,3,4,3]
               },
              {"drinkName": "CustomDrink3",
               "drinkID": 3,
               "drinkList": ["Juice","Tea","Coffee","Cider","Sodar","Water"],
-              "drinkOrder": [20,123,12,45,23,44]
+              "drinkOrder": [20,123,12,45,23,44],
+              "timesPressed": [1,2,1,3,4,3]
               },
              {"drinkName": "CustomDrink4",
               "drinkID": 4,
               "drinkList": ["Juice","Tea","Coffee","Cider","Sodar","Water"],
-              "drinkOrder": [20,123,12,45,23,44]
+              "drinkOrder": [20,123,12,45,23,44],
+              "timesPressed": [1,2,1,3,4,3]
               },
              {"drinkName": "CustomDrink5",
               "drinkID": 5,
               "drinkList": ["Juice","Tea","Coffee","Cider","Sodar","Water"],
-              "drinkOrder": [20,123,12,45,23,44]
+              "drinkOrder": [20,123,12,45,23,44],
+              "timesPressed": [1,2,1,3,4,3]
               },
              {"drinkName": "CustomDrink6",
               "drinkID": 6,
               "drinkList": ["Juice","Tea","Coffee","Cider","Sodar","Water"],
-              "drinkOrder": [20,123,12,45,23,44]
+              "drinkOrder": [20,123,12,45,23,44],
+              "timesPressed": [1,2,1,3,4,3]
               },]
               
 drinkQue = myQueue.queue()
 drinkDataInput = dummyData[0]
-for i in drinkDataInput["drinkOrder"]:
-    drinkQue.enqueue(i)
+for i in range(len(drinkDataInput["drinkOrder"])):
+    drinkQue.enqueue([drinkDataInput["drinkOrder"][i], drinkDataInput["timesPressed"][i]])
 
 print(drinkQue.get_data())
     
