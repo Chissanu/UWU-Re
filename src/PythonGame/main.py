4795827f7ac35d004cc553cf08452f3838a8c1ee
unknown <chissanuoak2@gmail.com>
2022-11-04 19:54:04 +0700

    Add bg image

--- src/PythonGame/main.py
+++ src/PythonGame/main.py
@@ -1,26 +1,32 @@
 import pygame
+import os
 
 WIDTH = 1920
 HEIGHT = 1080
 SCREEN = (WIDTH,HEIGHT)
 
+CURRENT_PATH = os.getcwd()
+BG_PATH = CURRENT_PATH + "\\src\\PythonGame\Assets\\Backgrounds"
+
 def main():
     pygame.init()
 
     # CREATING CANVAS
-    canvas = pygame.display.set_mode(SCREEN)
-    
+    window = pygame.display.set_mode(SCREEN)
     # TITLE OF CANVAS
     pygame.display.set_caption("UWU:Re")
-    exit = False
+    bg_img = pygame.image.load(BG_PATH + "\\grass.jpg")
+    bg_img = pygame.transform.scale(bg_img,(WIDTH,HEIGHT))
+    running = True
     
-    while not exit:
+    while running:
+        window.blit(bg_img,(0,0))
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
-                    exit = True
+                    running = False
             if event.type == pygame.QUIT:
-                exit = True
+                running = False
         pygame.display.update()
         
 
