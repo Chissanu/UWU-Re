b49b756e63bdf3636593c085f2254c5ac14c58ef
Phanuruj3146 <Phanuruj3146@gmail.com>
2022-11-07 02:32:33 +0700

    adjust bg

--- src/PythonGame/Classes/Character.py
+++ src/PythonGame/Classes/Character.py
@@ -4,13 +4,13 @@ from pygame.locals import *
 class Character(pygame.sprite.Sprite):
     def __init__(self, type):
         super().__init__()
-        self.walk_anim = [
-            pygame.image.load(os.path.join("Assets", type, f"{type}_Standing.png")).convert_alpha(),
-            pygame.image.load(os.path.join("Assets", type, f"{type}_L_Step.png")).convert_alpha(),
-            pygame.image.load(os.path.join("Assets", type, f"{type}_Standing.png")).convert_alpha(),
-            pygame.image.load(os.path.join("Assets", type, f"{type}_R_Step.png")).convert_alpha(),
-        ]
-        self.surface = pygame.Surface((100,150))
+        # self.walk_anim = [
+        #     pygame.image.load(os.path.join("Assets", type, f"{type}_Standing.png")).convert_alpha(),
+        #     pygame.image.load(os.path.join("Assets", type, f"{type}_L_Step.png")).convert_alpha(),
+        #     pygame.image.load(os.path.join("Assets", type, f"{type}_Standing.png")).convert_alpha(),
+        #     pygame.image.load(os.path.join("Assets", type, f"{type}_R_Step.png")).convert_alpha(),
+        # ]
+        # self.surface = pygame.Surface((100,150))
+        self.char_type = type
         self.direction = 1 #1 is 'right', 0 is 'left'
         self.step_count = 0
-        
--- src/PythonGame/main.py
+++ src/PythonGame/main.py
@@ -6,26 +6,41 @@ from Classes import Button
 pygame.init()
 
 SIZE = WIDTH, HEIGHT = (1920, 1080)
-DISPLAY = pygame.display.set_mode(SIZE)
-FPS = pygame.time.Clock()
+
+screen = pygame.display.set_mode(SIZE)
+pygame.display.set_caption('UwU:RE')
+start_game = False
+
+#set framerate
+clock = pygame.time.Clock()
+FPS = 60
+
+#define player action variables
+moving_left = False
+moving_right = False
+
+#define colors
+GREEN = (124,252,0)
+
+#load images
 CURRENT_PATH = os.getcwd()
+#background
 BG_PATH = CURRENT_PATH + "\\src\\PythonGame\\Assets\\Background"
-BTN_PATH = CURRENT_PATH + "\\src\\PythonGame\\Assets\\ButtonAssets"
-
-
 bg_img = pygame.image.load(BG_PATH + "\\grass.jpg")
 bg_img = pygame.transform.scale(bg_img,SIZE)
+#button images
+BTN_PATH = CURRENT_PATH + "\\src\\PythonGame\\Assets\\Button_img"
 start_img = pygame.image.load(BTN_PATH + "\\start_btn.png")
 exit_img = pygame.image.load(BTN_PATH + "\\exit_btn.png")
 
-
+#create buttons
 start_button = Button.Button(WIDTH/4 - start_img.get_width()/2, HEIGHT/2, start_img, 1.5)
 exit_button = Button.Button(WIDTH/2 + WIDTH/4 - exit_img.get_width()/2, HEIGHT/2, exit_img, 1.5)
 
 #Drawing the entire frame
 def draw_window(display, background):
     display.blit(background,(0,0))
-    pygame.display.update()
+
 
 #=====INITIALIZE======
 
@@ -34,17 +49,26 @@ def draw_window(display, background):
 
 running = True
 while running:
+
+    clock.tick(FPS)
+
+    if start_game == False:
+        screen.fill(GREEN)
+        # draw_window(screen, bg_img)
+        if start_button.draw(screen):
+            start_game = True
+        if exit_button.draw(screen):
+            running = False
+    else:
+        draw_window(screen, bg_img)
+
     for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+                running = False
+        #keyboard presses
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 running = False
-    
-    draw_window(DISPLAY,bg_img)
-
-    if start_button.draw(bg_img):
-        print('START')
-    if exit_button.draw(bg_img):
-        print('EXIT')
-
-    FPS.tick(60)
+                
+    pygame.display.update()
     
