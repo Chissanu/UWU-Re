403e0ed3526042fa77994958bbfbc7e2e5c33181
unknown <chissanuoak2@gmail.com>
2022-11-06 16:35:23 +0700

    Add .expo to gitignore

--- Mobile/App.js
+++ Mobile/App.js
@@ -4,18 +4,41 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>Hello World</Text>
+      <Text style={styles.UwU}>
+        UwU:
+        <Text style={styles.Re}>Re</Text>
+      </Text>
+      <Text style>Hello</Text>
       <StatusBar style="auto" />
     </View>
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#859FFD',
     alignItems: 'center',
-    justifyContent: 'center',
   },
+  UwU: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 100,
+    fontFamily: 'inter',
+    position: 'absolute',
+    top:'15%',
+  },
+  Re: {
+    color: 'black',
+    fontWeight: 'bold',
+    fontSize: 100,
+    fontFamily: 'inter',
+    top: '0%',
+  },
+  lineStyle:{
+    borderWidth: 0.5,
+    borderColor:'black',
+    margin:100,
+  }
 });
