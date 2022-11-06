bf3c521e6d3daf4aad2eedf647a054f14868b3b5
Phanuruj3146 <Phanuruj3146@gmail.com>
2022-11-06 20:15:47 +0700

    add button class file and test it in the main.py

--- src/PythonGame/main.py
+++ src/PythonGame/main.py
@@ -1,6 +1,7 @@
 import pygame
 import os
-from Classes import Player
+from Classes import Button
+# from Classes import Player
 
 pygame.init()
 
@@ -9,11 +10,17 @@ DISPLAY = pygame.display.set_mode(SIZE)
 FPS = pygame.time.Clock()
 CURRENT_PATH = os.getcwd()
 BG_PATH = CURRENT_PATH + "\\src\\PythonGame\\Assets\\Background"
+BTN_PATH = CURRENT_PATH + "\\src\\PythonGame\\Assets\\ButtonAssets"
+
 
 bg_img = pygame.image.load(BG_PATH + "\\grass.jpg")
 bg_img = pygame.transform.scale(bg_img,SIZE)
+start_img = pygame.image.load(BTN_PATH + "\\start_btn.png")
+exit_img = pygame.image.load(BTN_PATH + "\\exit_btn.png")
 
 
+start_button = Button.Button(WIDTH/4 - start_img.get_width()/2, HEIGHT/2, start_img, 1.5)
+exit_button = Button.Button(WIDTH/2 + WIDTH/4 - exit_img.get_width()/2, HEIGHT/2, exit_img, 1.5)
 
 #Drawing the entire frame
 def draw_window(display, background):
@@ -23,7 +30,7 @@ def draw_window(display, background):
 #=====INITIALIZE======
 
 #User Events
-player = Player()
+# player = Player()
 
 running = True
 while running:
@@ -33,5 +40,11 @@ while running:
                 running = False
     
     draw_window(DISPLAY,bg_img)
+
+    if start_button.draw(bg_img):
+        print('START')
+    if exit_button.draw(bg_img):
+        print('EXIT')
+
     FPS.tick(60)
     
