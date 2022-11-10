dfde666e891625aa5eb6d41bdba5bae3f67a163c
Tonkhaowm8 <Tonkhaowm8@gmail.com>
2022-11-11 01:55:38 +0700

    add motor control

--- src/arduino/motor_control/motor_control.ino
+++ src/arduino/motor_control/motor_control.ino
@@ -1,78 +1,60 @@
+#include <Stepper.h>
+
 // UWU-RE 2022
 
+// Number of steps per revolution
 const int stepsPerRevolution = 200;
-// pin setup
-const int x_step = A0;
-const int x_dir = A1;
-const int z_step = 46;
-const int z_dir = 48;
-const int x_min = 2;
-const int x_max = 3;
-const int z_min = 18;
-const int z_max = 19;
 
-// end stop
-int x_min_stop = 0;
-int x_max_stop = 0;
-int z_min_stop = 0;
-int z_max_stop = 0;
+// Initiate Stepper
+Stepper x_stepper(stepsPerRevolution, 1, 2, 3, 4);
+Stepper z_stepper(stepsPerRevolution, 5, 6, 7, 8);
 
-//setup pinmode and stuff
+// Initiate End stops
+#define xMinPin 9
+#define xMaxPin 10
+#define zMinPin 11
+#define zMaxPin 12
+
+int x_min;
+int x_max;
+int z_min;
+int z_max;
+
 void setup(){
-  pinMode(x_step, OUTPUT);
-  pinMode(x_dir, OUTPUT);
-  pinMode(z_step, OUTPUT);
-  pinMode(z_dir, OUTPUT);
-  pinMode(x_min, INPUT);
-  pinMode(x_max, INPUT);
-  pinMode(z_min, INPUT);
-  pinMode(z_max, INPUT);
-
+  // set serial port to 9600
   Serial.begin(9600);
-  
+  Serial.println("ready");
+
+  // set speed of stepper motor
+  x_stepper.setSpeed(60);
+  z_stepper.setSpeed(60);
+
+  // set mode of endstop
+  pinMode(xMinPin, INPUT);
+  pinMode(xMaxPin, INPUT);
+  pinMode(zMinPin, INPUT);
+  pinMode(zMaxPin, INPUT);
 }
 
 void loop(){
   while (Serial.available()){
     String input = Serial.readString();
-    Serial.print(input.length());
-    //calibrate(input);
+    input.trim();
+    if (input.substring(0, 1) == "c"){
+      Serial.println("Calibrate");
+      calibrate(input.substring(1), , x_stepper, z_stepper);
+    }
   }
 }
 
-void calibrate(char axis, int ){
-  Serial.println(axis);
-  
-  // Calibrate X-axis
-  if(strcmp(axis, "c1")){
-    Serial.println("Hello");
-    int total_x_steps = 0;
+void calibrate(String axis, Stepper x_stepper, Stepper z_stepper){
+  // calibrate both sides
+  if (axis == "1"){
+    Serial.println("Calibrating Both Axis");
+
+    // x - axis
+    while (digitalRead())
+    x_stepper.step(1); // set direction
     
-    // Start from 0
-    x_min_stop = digitalRead(x_min);
-    digitalWrite(x_dir, HIGH); //set to clockwise
-    while(x_min_stop != true){
-      digitalWrite(x_step, HIGH);
-      delay(500);
-      digitalWrite(x_step, LOW);
-      delay(500);
-      x_min_stop = digitalRead(x_min);
-    }
-    digitalWrite(x_step, LOW);
-
-    //Count Steps
-    x_max_stop = digitalRead(x_max);
-    digitalWrite(x_dir, LOW); // set to clockwise
-    while (x_max_stop != true){
-      digitalWrite(x_step, HIGH);
-      delay(500);
-      digitalWrite(x_step, LOW);
-      delay(500);
-      total_x_steps = total_x_steps + 1; //adding the total steps
-      x_max_stop = digitalRead(x_max);
-    }
-    digitalWrite(x_step, LOW);
-
-    //
   }
 }
