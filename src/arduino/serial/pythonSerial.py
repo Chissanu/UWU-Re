236e583f21023e4a8266d580c4613b94618f8595
chissanu <chissanuoak2@gmail.com>
2022-11-28 04:41:50 +0700

    Add python serial

---
+++ src/arduino/serial/pythonSerial.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import serial
+import time
+from src.PythonTkinter.dispense_drink import DispenseDrink
+
+
+if __name__ == '__main__':
+    ser = serial.Serial('/dev/ttyACM0',9600,timeout=1)
+    ser.reset_input_buffer()
+
+    while True:
+        ser.write(b"Hello from Raspberry Pi!\n")
+        line = ser.readline().decode('utf-8').rstrip()
+        print(line)
+        time.sleep(1)
